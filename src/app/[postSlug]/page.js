daddd2cce602903b9e022d91b61d97ad6de63269
PhanTrongHau <haupk2003@gmail.com>
2024-08-16 02:08:54 +0700

    solution

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import BlogHero from '@/components/BlogHero';
+import BlogHero from "@/components/BlogHero";
 
-import styles from './postSlug.module.css';
+import styles from "./postSlug.module.css";
+import { loadBlogPost } from "@/helpers/file-helpers";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import CodeSnippet from "@/components/CodeSnippet";
+import DivisionGroupsDemo from "@/components/DivisionGroupsDemo";
+import CircularColorsDemo from "@/components/CircularColorsDemo";
 
-function BlogPost() {
+export async function generateMetadata({ params }) {
+  const { postSlug } = params;
+  const { frontmatter } = await loadBlogPost(postSlug);
+
+  return {
+    title: frontmatter.title,
+    description: frontmatter.description,
+  };
+}
+
+async function BlogPost({ params }) {
+  const { postSlug } = params;
+  const { frontmatter, content } = await loadBlogPost(postSlug);
+
+  console.log({ frontmatter, content });
+
   return (
     <article className={styles.wrapper}>
-      <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
-      />
+      <BlogHero {...frontmatter} />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
+        <MDXRemote
+          source={content}
+          components={{
+            pre: CodeSnippet,
+            DivisionGroupsDemo,
+            CircularColorsDemo,
+          }}
+        />
       </div>
     </article>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import clsx from 'clsx';
-import { Rss, Sun, Moon } from 'react-feather';
+"use client";
+import React from "react";
+import clsx from "clsx";
+import { Rss, Sun, Moon } from "react-feather";
 
-import Logo from '@/components/Logo';
-import VisuallyHidden from '@/components/VisuallyHidden';
+import Logo from "@/components/Logo";
+import VisuallyHidden from "@/components/VisuallyHidden";
 
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
+import Cookies from "js-cookie";
+import { DARK_TOKENS, LIGHT_TOKENS } from "@/constants";
 
 function Header({ theme, className, ...delegated }) {
+  const [statusTheme, setStatusTheme] = React.useState(theme);
+
   return (
-    <header
-      className={clsx(styles.wrapper, className)}
-      {...delegated}
-    >
+    <header className={clsx(styles.wrapper, className)} {...delegated}>
       <Logo />
 
       <div className={styles.actions}>
@@ -21,18 +23,35 @@ function Header({ theme, className, ...delegated }) {
             size="1.5rem"
             style={{
               // Optical alignment
-              transform: 'translate(2px, -2px)',
+              transform: "translate(2px, -2px)",
             }}
           />
-          <VisuallyHidden>
-            View RSS feed
-          </VisuallyHidden>
+          <VisuallyHidden>View RSS feed</VisuallyHidden>
         </button>
-        <button className={styles.action}>
-          <Sun size="1.5rem" />
-          <VisuallyHidden>
-            Toggle dark / light mode
-          </VisuallyHidden>
+        <button
+          className={styles.action}
+          onClick={() => {
+            const newTheme = statusTheme === "light" ? "dark" : "light";
+
+            setStatusTheme(newTheme);
+            Cookies.set("theme", newTheme, {
+              expires: 365,
+            });
+
+            const tokens = newTheme === "light" ? LIGHT_TOKENS : DARK_TOKENS;
+
+            document.documentElement.setAttribute("data-color-theme", newTheme);
+            Object.entries(tokens).forEach(([key, value]) => {
+              document.documentElement.style.setProperty(key, value);
+            });
+          }}
+        >
+          {statusTheme === "light" ? (
+            <Sun size="1.5rem" />
+          ) : (
+            <Moon size="1.5rem" />
+          )}
+          <VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
         </button>
       </div>
     </header>
